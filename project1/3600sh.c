493a60bfd86fea4c00353f466056068cc0d78390
Benjamin J. Lieberman <bliebs@tekken.ccs.neu.edu>
2014-01-25 18:40:15 -0500

    fixed mislove test script segfault problem, implemented all features of milestone except redirection

--- project1/3600sh.c
+++ project1/3600sh.c
@@ -1,14 +1,14 @@
 #include "3600sh.h"
 
-#define MAXLINE 200  /* This is how we declare constants in C */
+#define MAXLINE 200  // Constants
 #define MAXARGS 20
 
 #define USE(x) (x) = (x)
 
 static char * getword(char * begin, char **end_ptr) {
   char * end = begin;
-  while ( *begin == ' ' )
-    begin++;  /* Get rid of leading spaces. */
+  while ( *begin == ' ' || *begin == '\t')
+    begin++;  /* Get rid of leading spaces and tabs */
     end = begin;
 	
   while ( *end != '\0' && *end != '\n' && *end != ' ' && *end != '#')
@@ -25,7 +25,7 @@ static char * getword(char * begin, char **end_ptr) {
 	  begin = "UNDEFINED";
     }
   }
-  return begin; /* This word is now a null-terminated string.  return it. */
+  return begin; /* This word is now a null-terminated string. return it. */
 }
 
 static void getargs(char cmd[], int *argcp, char *argv[]) {
@@ -35,23 +35,24 @@ static void getargs(char cmd[], int *argcp, char *argv[]) {
 
   /* fgets creates null-terminated string. stdin is pre-defined C constant
    *   for standard intput.  feof(stdin) tests for file:end-of-file.
-   */
-  if (fgets(cmd, MAXLINE, stdin) == NULL && feof(stdin)) {
-    printf("Couldn't read from standard input. End of file? Exiting ...\n");
-    do_exit();
-  }
+	*/
+  if (fgets(cmd, MAXLINE, stdin) == NULL) {
+		do_exit();
+	}
+		
   while ( (cmdp = getword(cmdp, &end)) != NULL ) { /* end is output param */
     argv[i++] = cmdp;
-    /* "end" brings us only to the '\0' at end of string */
-	cmdp = end + 1;
-    }
-    argv[i] = NULL; /* Create additional null word at end for safety. */
-    *argcp = i;
+		cmdp = end + 1;
+	}
+	
+	argv[i] = NULL; /* Create additional null word at end for safety. */
+	*argcp = i;
 }
 
 static void execute (char *argv[]) {
   pid_t childpid;
   childpid = fork();
+	
   if (childpid == -1) {
     perror("fork");
     printf("Failed to execute command.\n");
@@ -59,13 +60,14 @@ static void execute (char *argv[]) {
   if (childpid == 0) {
     if (-1 == execvp(argv[0], argv)) {
       printf("Error: Command not found.\n");
+			exit(0);
     }
-    do_exit();
+		do_exit();
   }
-    else //parent: in parent, childpid was set to pid of child process
-      waitpid(childpid, NULL, 0);
-  
-  return;
+	
+	else //parent: in parent, childpid was set to pid of child process
+		waitpid(childpid, NULL, 0);
+	  //do_exit();
 }
 
 void interrupt_handler() {
@@ -76,29 +78,30 @@ int main(int argc, char *argv[]) {
 	USE(argc);
 	USE(argv);
 	setvbuf(stdout, NULL, _IONBF, 0); 
-
-    void interrupt_handler();
-
-    char cmd[MAXLINE];
-    char *childargv[MAXARGS];
-    int childargc;
+	
+	void interrupt_handler();
+  char cmd[MAXLINE] = "";
+  char *childargv[MAXARGS];
+  int childargc = 0;
 
 	signal(SIGINT, interrupt_handler);
 	
-	char hostname[1024];
-	gethostname(hostname, 1024);
-
-    while (1) {
-	  printf("%s@%s:%s> ", getenv("USER"),hostname,getenv("PWD"));
-	  fflush(stdout); /* flush from output buffer to terminal itself */
-	  getargs(cmd, &childargc, childargv);
-	
+	char hostname[512] = "";
+	gethostname(hostname, 512);
+		
+	while (!feof(stdin)) {
+		
+		printf("%s@%s:%s> ", getenv("USER"),hostname,getenv("PWD"));
+		getargs(cmd, &childargc, childargv);
+		
 	  if ( childargc > 0 && strcmp(childargv[0], "exit") == 0 )
 	    do_exit();
-      else
+	  else
 	    execute(childargv);
-      }
-	return 0;
+	}
+	
+	do_exit();
+  return 0;
 }
 
 void do_exit() {
